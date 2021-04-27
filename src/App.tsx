13b861ff809d66a9e184af0e09952b95aa5a0940
FedericoAvano <federico.m.avano@gmail.com>
2021-04-27 21:28:11 +0200

    Table working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
 import React from 'react';
 import MyTable from './Components/MyTable'
-import './App.css';
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import PageHeader from './Components/PageHeader';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides:{
+    MuiAppBar:{
+      root:{
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple:true
+    }
+  }
+})
+
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    width: '100%'
+  }
+})
+
 
 function App() {
+  const classes =useStyles();
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+    <div className={classes.appMain}>
+      <PageHeader title="Tab Test" subTitle="Material UI Table test" />
       <MyTable />
-     
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/Components/MyTable.tsx
+++ src/Components/MyTable.tsx
@@ -1,48 +1,52 @@
 import * as React from 'react';
-import Button from '@material-ui/core/Button';
-import { DataGrid, GridColDef, GridCellParams } from '@material-ui/data-grid';
+import { DataGrid, GridColDef, GridSortCellParams, GridCellParams } from '@material-ui/data-grid';
+import { Button } from '@material-ui/core';
+
+function getFullName(params: GridSortCellParams) {
+  return `${params.getValue('firstName') || ''} ${
+    params.getValue('lastName') || ''
+  }`;
+}
 
 const columns: GridColDef[] = [
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
   {
-    field: 'date',
-    headerName: 'Year',
-    width: 150,
-    renderCell: (params: GridCellParams) => (
-      <strong>
-        {(params.value as Date).getFullYear()}
-        <Button
-          target ="_self"
-          variant="contained"
-          color="primary"
-          size="small"
-          href="https://reactjs.org/"
-          style={{ marginLeft: 16 }}
-        >
-          Open
-        </Button>
-      </strong>
-    ),
+    field: 'fullName',
+    headerName: 'Full name',
+    width: 500,
+    valueGetter: getFullName,
+    sortComparator: (v1, v2, cellParams1, cellParams2) =>
+      getFullName(cellParams1).localeCompare(getFullName(cellParams2)),
   },
+  { field: 'Check', headerName: 'Check Data', width: 150,
+  renderCell: (params: GridCellParams) => (
+    <strong>
+      {(params.value as GridColDef)}
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        style={{ marginLeft: 0 }}
+      >
+       Credentials
+      </Button>
+    </strong>
+  ),
+  }
 ];
 
 const rows = [
-  {
-    id: 1,
-    date: new Date(1979, 0, 1),
-  },
-  {
-    id: 2,
-    date: new Date(1984, 1, 1),
-  },
-  {
-    id: 3,
-    date: new Date(1992, 2, 1),
-  },
+  { id: 1, lastName: 'Rossi', firstName: 'Mario' },
+  { id: 2, lastName: 'Avano ', firstName: 'Federico' },
+  { id: 3, lastName: 'Pippo', firstName: 'Valore' },
+  { id: 4, lastName: 'Moog', firstName: 'Robert' },
+  { id: 5, lastName: 'Eastwood', firstName: 'Clint' },
 ];
 
-export default function RenderCellGrid() {
+export default function ValueGetterGrid() {
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div style={{ height: 400, width: '100%' }}>
       <DataGrid rows={rows} columns={columns} />
     </div>
   );
