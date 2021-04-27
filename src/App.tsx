20c04a53373ab3b00a5286ed05b4c13400881172
FedericoAvano <federico.m.avano@gmail.com>
2021-04-27 17:42:38 +0200

    Proviamo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyTable from './Components/MyTable'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyTable />
+     
+    </div>
+  );
+}
+
+export default App;
